d6e1638bc203e24b427433ee80d70c6990e907ce
Atul Anand <atul@Atuls-MacBook-Air.local>
2025-03-03 13:26:04 +0530

    modified about section added some components

--- starter-template/src/sections/Projects.tsx
+++ starter-template/src/sections/Projects.tsx
@@ -6,6 +6,8 @@ import CheckCircleIcon from "@/assets/icons/check-circle.svg";
 import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
 import grainImage from "@/assets/images/grain.jpg";
 import { div, span } from "framer-motion/client";
+import { SectionHeader } from "@/components/SectionHeader";
+import { Card } from "@/components/Card";
 
 const portfolioProjects = [
   {
@@ -50,33 +52,15 @@ export const ProjectsSection = () => {
   return (
   <section className="pb-16 lg:py-24">
     <div className="container">
-      <div className="flex justify-center">
-      <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400
-         bg-clip-text text-transparent">
-        Real-World-Results
-        </p>
-        </div>
-      <h2 className="font-serif text-3xl md:text-5xl text-center mt-6">
-        Featured Projects
-        </h2>
-      <p className="text-center md:text-lg lg:text-xl text-white/60 mt-4 max-w-md
-        mx-auto">
-        See How I transformed concepts into engaging digita expereinces
-        </p>
+      <SectionHeader eyebrow="Real-World-Results" title="Featured Projects" description="See How I transformed concepts into engaging digita expereinces"/>
+      
     
     <div className="mt-10 md:mt-20 flex flex-col gap-20">
       {portfolioProjects.map(project=>(
-        <div key={project.title} 
-        className="bg-gray-800 rounded-3xl relative z-0 
-        overflow-hidden after:z-10 after:content-[''] after:absolute after:inset-0 
-        after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl
-        after:outline-white/20 px-8 pt-8 md:pt-12 md:px-10 lg:pt-16 lg:px-20 after:pointer-events-none
+        <Card key={project.title} 
+        className="px-8 pt-8 pb-0 md:pt-12 md:px-10 lg:pt-16 lg:px-20
         ">
-          <div className="absolute inset-0 -z-10 opacity-5" 
-          style={{
-            backgroundImage: `url(${grainImage.src})`,
-          }}
-          ></div>
+          
           <div className="lg:grid lg:grid-cols-2
           lg:gap-16">
             <div className="lg:pb-16">
@@ -116,7 +100,7 @@ export const ProjectsSection = () => {
           lg:mt-0 lg:absolute lg:h-full lg:w-auto lg:max-w-none"/>
           </div>
           </div>
-          </div>
+          </Card>
       ))}
     </div>
   </div>
